7a036d92ec505ad4b851a71628089f8299a53b55
Saul QIAN <lennon@imqx.com>
2019-06-17 21:46:43 +0800

    change compiler to babel

---
+++ main.js
@@ -0,0 +1,65 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _conversion = require("./lib/utilities/conversion");
+
+Object.keys(_conversion).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function () {
+      return _conversion[key];
+    }
+  });
+});
+
+var _crypto = require("./lib/utilities/crypto");
+
+Object.keys(_crypto).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function () {
+      return _crypto[key];
+    }
+  });
+});
+
+var _factory = require("./lib/utilities/factory");
+
+Object.keys(_factory).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function () {
+      return _factory[key];
+    }
+  });
+});
+
+var _polyfill = require("./lib/utilities/polyfill");
+
+Object.keys(_polyfill).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function () {
+      return _polyfill[key];
+    }
+  });
+});
+
+var _system = require("./lib/utilities/system");
+
+Object.keys(_system).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function () {
+      return _system[key];
+    }
+  });
+});
