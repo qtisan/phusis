5a9543d58816d5ea44a6215b471569ec06c2b322
Saul QIAN <lennon@imqx.com>
2019-06-27 20:38:11 +0800

    fix the bug in `encrypt()`, error test hex.

--- test/lib/utilities/crypto.spec.js
+++ test/lib/utilities/crypto.spec.js
@@ -7,9 +7,14 @@ describe('Is crypto functions works correct?', () => {
   const encryptedCode = '=yM4IyMyIyNtb2VyYWVfYXBwX2NpcGhlcg=I.rE-lH1xhHZ79BABUCrQARQ';
   const md5Code = 'caecb26de1c989826750c7c478a9401d';
 
+  const originEase = '123';
+  const encryptedEase = '=yM2IyMyIyNtb2VyYWVfYXBwX2NpcGhlcg=I.QmXIBm';
+
   it('should the encrypt be right!', () => {
     expect(encrypt(originCode)).toEqual(encryptedCode);
     expect(decrypt(encryptedCode)).toEqual(originCode);
+    expect(encrypt(originEase)).toEqual(encryptedEase);
+    expect(decrypt(encryptedEase)).toEqual(originEase);
   });
 
   it('should the md5 function right?', () => {
