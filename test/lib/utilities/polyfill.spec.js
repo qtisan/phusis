83da639074f10d72642a99fc8891b4423ff7d103
Saul QIAN <lennon@imqx.com>
2019-07-12 23:15:14 +0800

    fix the test suit.

--- test/lib/utilities/polyfill.spec.js
+++ test/lib/utilities/polyfill.spec.js
@@ -2,14 +2,14 @@ import '../../..';
 import { caught } from '../../..';
 
 describe('Would these be the build-in functions?', () => {
-
-  it('Date.prototype.getStamp()', () => { 
+  it('Date.prototype.getStamp()', () => {
     const d = new Date('2008-8-8');
     expect(d.getStamp()).toEqual(1218124800);
   });
 
   it('String.prototype.isCnNewID()', () => {
-    const str = '189473', newid = '330382198608110018';
+    const str = '189473',
+      newid = '330382198608110018';
     expect(str.isCnNewID()).not.toBeTruthy();
     expect(newid.isCnNewID()).toBeTruthy();
   });
@@ -17,10 +17,9 @@ describe('Would these be the build-in functions?', () => {
   it('caught()', () => {
     const error = new Error('I am an error!');
     const newError = caught('I hide the real error!');
-    const newEntireError = caught({ message: 'I DID hide the real error!', code: 109, type: 'sys' });
-    expect(newError).toMatchSnapshot();
+    const newEntireError = caught(error, 'I DID hide the real error!', 109);
+    expect(newError instanceof Error).toBeTruthy();
     expect(newEntireError).toMatchSnapshot();
     // newEntireError.log();
   });
-
 });
